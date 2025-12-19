f86308dfed1c083d972edbe00e7fd15862b696dd
minazuki_666 <junmen.abe@gmail.com>
2025-12-19 20:30:42 +0900

    追加

---
+++ tool_csvToTableツール.py
@@ -0,0 +1,20 @@
+#%%
+import os
+import numpy as np
+
+# 対象path
+path = './tables/subjectFeature.csv'
+
+# 文字列作成
+df = np.loadtxt(path, delimiter=',', dtype=str)
+
+sentence = []
+for i in range(len(df)):
+  row = df[i]
+  sentence.append(f"{'],['.join(row)}")  # 各文字列を [] で囲む
+
+# 文字列出力
+sentence = '],\n['.join(sentence)
+sentence = f'[{sentence}],'
+print(sentence)  # 各行を改行で区切る
+# %%
